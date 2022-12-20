ba5e442a47657c922b58968724f493901cef613b
Sumudu Prabasitha <Sumudu.Prabasitha@cambio.lk>
2022-12-20 15:07:23 +0530

    Initial commit

---
+++ grott.py
@@ -0,0 +1,53 @@
+#Grott Growatt monitor based on TCPIP sniffing or proxy (new 2.0) 
+#             
+#       Monitor needs to run on a (linux) system that is abble to see TCPIP that is sent from inverter to Growatt Server
+#       
+#       In the TCPIP sniffer mode this can be achieved by rerouting the growatt WIFI data via a Linux server with port forwarding
+#
+#       For more information how to see aditional documentation on github 
+#
+#       Monitor can run in forground and as a standard service!
+#
+#       For version history see: version_history.txt
+
+# Updated: 2021-12-14'
+
+verrel = "2.6.1h"
+
+import sys
+
+from grottconf import Conf
+from grottproxy import Proxy
+from grottsniffer import Sniff
+
+#proces config file
+conf = Conf(verrel)
+
+#print configuration
+if conf.verbose: conf.print()
+
+#To test config only remove # below
+#sys.exit(1)
+
+if conf.mode == 'proxy':
+        proxy = Proxy(conf)
+        try:
+            proxy.main(conf)
+        except KeyboardInterrupt:
+            print("Ctrl C - Stopping server")
+            try: 
+                proxy.on_close(conf)
+            except:     
+                print("\t - no ports to close")
+            sys.exit(1)
+
+if conf.mode == 'sniff':
+        sniff = Sniff(conf)
+        try: 
+            sniff.main(conf)
+        except KeyboardInterrupt:
+            print("Ctrl C - Stopping server")
+            sys.exit(1)
+
+else:
+    print("- Grott undefined mode")
---
+++ grottproxy.py
@@ -0,0 +1,168 @@
+#Grott Growatt monitor :  Proxy 
+#       
+# Updated: 2020-10-04
+# Version 2.2.1d
+
+import socket
+import select
+import time
+import sys
+import struct
+import textwrap
+from itertools import cycle # to support "cycling" the iterator
+import time, json, datetime, codecs
+## to resolve errno 32: broken pipe issue (only linux)
+if sys.platform != 'win32' :
+   from signal import signal, SIGPIPE, SIG_DFL
+
+from grottdata import procdata, decrypt
+
+#import mqtt                       
+import paho.mqtt.publish as publish
+
+# Changing the buffer_size and delay, you can improve the speed and bandwidth.
+# But when buffer get to high or delay go too down, you can broke things
+buffer_size = 4096
+#buffer_size = 65535
+delay = 0.0002
+
+class Forward:
+    def __init__(self):
+        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
+        try:
+            self.forward.connect((host, port))
+            return self.forward
+        except Exception as e:
+            print(e)
+            return False  
+
+class Proxy:
+    input_list = []
+    channel = {}
+
+    def __init__(self, conf):
+        print("\nGrott proxy mode started")
+        ## to resolve errno 32: broken pipe issue (Linux only)
+        if sys.platform != 'win32':
+            signal(SIGPIPE, SIG_DFL) 
+        ## 
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #set default grottip address
+        if conf.grottip == "default" : conf.grottip = '0.0.0.0'
+        self.server.bind((conf.grottip, conf.grottport))
+        #socket.gethostbyname(socket.gethostname())
+        try: 
+            hostname = (socket.gethostname())    
+            print("Hostname :", hostname)
+            print("IP : ", socket.gethostbyname(hostname), ", port : ", conf.grottport, "\n")
+        except:  
+            print("IP and port information not available") 
+
+        self.server.listen(200)
+        self.forward_to = (conf.growattip, conf.growattport)
+        
+    def main(self,conf):
+        self.input_list.append(self.server)
+        while 1:
+            time.sleep(delay)
+            ss = select.select
+            inputready, outputready, exceptready = ss(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept(conf)
+                    break
+                try: 
+                    self.data, self.addr = self.s.recvfrom(buffer_size)
+                except: 
+                    if conf.verbose : print("\t - Grott connection error")    
+                if len(self.data) == 0:
+                    self.on_close(conf)
+                    break
+                else:
+                    self.on_recv(conf)
+
+    def on_accept(self,conf):
+        forward = Forward().start(self.forward_to[0], self.forward_to[1])
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            if conf.verbose: print("\t -", clientaddr, "has connected")
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = forward
+            self.channel[forward] = clientsock
+        else:
+            if conf.verbose: 
+                print("\t - Can't establish connection with remote server."),
+                print("\t - Closing connection with client side", clientaddr)
+            clientsock.close()
+
+    def on_close(self,conf):
+        if conf.verbose: 
+            #try / except to resolve errno 107: Transport endpoint is not connected 
+            try: 
+                print("\t -", self.s.getpeername(), "has disconnected")
+            except:  
+                print("\t -", "peer has disconnected")
+
+        #remove objects from input_list
+        self.input_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s])
+        out = self.channel[self.s]
+        # close the connection with client
+        self.channel[out].close()  # equivalent to do self.s.close()
+        # close the connection with remote server
+        self.channel[self.s].close()
+        # delete both objects from channel dict
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self,conf):
+        data = self.data      
+        print("")
+        print("\t - " + "Growatt packet received:") 
+        print("\t\t ", self.channel[self.s])
+        # FILTER!!!!!!!! Detect if configure data is sent!
+        header = "".join("{:02x}".format(n) for n in data[0:8])
+        if conf.blockcmd : 
+            #standard everything is blocked!
+            blockflag = True 
+            #partly block configure Shine commands                   
+            if header[14:16] == "18" :         
+                if conf.blockcmd : 
+                    if header[6:8] == "05" or header[6:8] == "06" : confdata = decrypt(data) 
+                    else :  confdata = data
+
+                    #get conf command (location depends on record type), maybe later more flexibility is needed
+                    if header[6:8] == "06" : confcmd = confdata[76:80]
+                    else: confcmd = confdata[36:40]
+                    
+                    if header[14:16] == "18" : 
+                        #do not block if configure time command of configure IP (if noipf flag set)
+                        if conf.verbose : print("\t - Grott: Shine Configure command detected")                                                    
+                        if confcmd == "001f" or (confcmd == "0011" and conf.noipf) : 
+                            blockflag = False
+                            if confcmd == "001f": confcmd = "Time"
+                            if confcmd == "0011": confcmd = "Change IP"
+                            if conf.verbose : print("\t - Grott: Configure command not blocked : ", confcmd)    
+                    else : 
+                        #All configure inverter commands will be blocked
+                        if conf.verbose : print("\t - Grott: Inverter Configure command detected")
+            
+            #allow records: 
+            if header[12:16] in conf.recwl : blockflag = False     
+
+            if blockflag : 
+                print("\t - Grott: Record blocked: ", header[12:16])
+                return
+
+        # send data to destination
+        self.channel[self.s].send(data)
+        if len(data) > conf.minrecl :
+            #process received data
+            procdata(conf,data)    
+        else:     
+            if conf.verbose: print("\t - " + 'Data less then minimum record length, data not processed') 
+                
